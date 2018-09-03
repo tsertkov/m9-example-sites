0cc0c0e8ac1afa09206ab67597033b16fc847e4c
Aleksandr Tsertkov <tsertkov@gmail.com>
2018-09-03 10:38:44 +0200

    Add missing comma

--- serverless-deploy/handlers.js
+++ serverless-deploy/handlers.js
@@ -1,6 +1,21 @@
 const m9 = require('@tsertkov/m9-generator')({
   babelCachePath: '/tmp/babel-cache.json',
-  dst: '/tmp/build'
+  dst: '/tmp/build',
+  function config (m9config) {
+    const { plugins } = m9config.content
+    const staticPluginIdx = plugins.findIndex(
+      plugin => plugin.name === 'json-dir'
+    )
+
+    // Replace json-dir plugin with wp-json
+    // to load current content from wp endpoint
+    plugins[staticPluginIdx] = {
+      name: 'wp-json',
+      options: m9config.content.wpJson
+    }
+
+    return m9config
+  }
 })
 
 module.exports.deploy = function deploy (event, context, callback) {
